f7f104978c43e8e39f596762ed2796cbcb57e5bb
Makwe-O <notmaks@yahoo.com>
2021-01-24 21:17:32 +0100

    update projects

--- src/data/data.js
+++ src/data/data.js
@@ -1,243 +1,275 @@
+import { v4 as uuidv4 } from "uuid";
 export const data = [
   {
-    id: 1,
-    name: `Author's haven`,
+    id: uuidv4(),
+    name: `Trust You(Messaging)`,
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1573508650/Untitled-12_dhnp9h.jpg',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1611518959/Screenshot_2021-01-24_at_21.06.49_zxshiq.png",
     description:
-      'An online platform for sharing ideas, thoughts and concepts with the world leveraging the power of the modern web. I worked as a Full Stack developer on this project. The MVP was built within 3 months',
+      "Trust you eases hotel management by turning communication into satisfaction, reviews into bookings complexity into peace of mind. I worked on a micro-frontend application allowing hotel staffs to communicate with each other thus easing the process of resolving issues",
     stacks: [
-      'React',
-      'Redux',
-      'Nodejs',
-      'Express',
-      'HTML5',
-      'CSS3',
-      'Semantic UI',
-      'JavaScript',
-      'Postgres',
+      "React",
+      "Redux",
+      "Styled-Components",
+      "SocketIO",
+      "StoryBook",
+      "JavaScript",
     ],
-    link: 'https://authors-haven-app-21.herokuapp.com/',
-    github: 'https://github.com/Makwe-O/Authors-Haven',
+    link: "https://www.trustyou.com/",
+    github: null,
   },
   {
-    id: 2,
-    name: 'Peerless LMS',
+    id: uuidv4(),
+    name: `FireFlies AI`,
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1587552456/peerless_hbd3gb.png',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1611518135/Screenshot_2021-01-24_at_20.54.26_morevg.png",
     description:
-      'An online Learning Management System that includes tools that your business needs to inspire continuous and collaborative learning in less time. Not only can it help your learners, but it can also significantly help transform your businesses in all aspects',
-    stacks: ['HTML5, CSS3, JavaScript, React, Bootstrap 4, Redux'],
-    link: 'https://peerlesslms.com/',
+      "An application that allows users to Record, Transcribe and Search across thier voice conversations. Worked on the client-facing UseNotes module of the application identifying bugs and issues on the UseNotes module as well as suggested improvements",
+    stacks: [
+      "React",
+      "GrapQl",
+      "MeteorJs",
+      "HTML5",
+      "CSS3",
+      "Pupeteer",
+      "JavaScript",
+    ],
+    link: "https://fireflies.ai/",
+    github: null,
+  },
+  {
+    id: uuidv4(),
+    name: `Author's haven`,
+    img:
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1573508650/Untitled-12_dhnp9h.jpg",
+    description:
+      "An online platform for sharing ideas, thoughts and concepts with the world leveraging the power of the modern web. I worked as a Full Stack developer on this project. The MVP was built within 3 months",
+    stacks: [
+      "React",
+      "Redux",
+      "Nodejs",
+      "Express",
+      "HTML5",
+      "CSS3",
+      "Semantic UI",
+      "JavaScript",
+      "Postgres",
+    ],
+    link: "https://authors-haven-app-21.herokuapp.com/",
+    github: "https://github.com/Makwe-O/Authors-Haven",
+  },
+  {
+    id: uuidv4(),
+    name: "Leanry LMS",
+    img:
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1611517855/Screenshot_2021-01-24_at_20.47.43_kaakfm.png",
+    description:
+      "An online Learning Management System that includes tools that your business needs to inspire continuous and collaborative learning in less time. Not only can it help your learners, but it can also significantly help transform your businesses in all aspects",
+    stacks: ["HTML5, CSS3, JavaScript, React, Bootstrap 4, Redux"],
+    link: "https://learnry.com/",
     github: null,
   },
   {
     id: 3,
     name: `Haxx Store`,
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1573508649/Untitled-3_exg4wo.jpg',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1573508649/Untitled-3_exg4wo.jpg",
     description:
-      'Haxx store is online store made to take over the likes of Asos. Little steps right?. Anyways online store to shop for various accessories. With payment integration. Dont worry its in test mode',
-    stacks: ['React', 'Redux', 'Firebase', 'HTML5', 'CSS3', 'JavaScript'],
-    link: 'https://haxxstore.netlify.com/',
-    github: 'https://github.com/Makwe-O/HaxxStore',
+      "Haxx store is online store made to take over the likes of Asos. Little steps right?. Anyways online store to shop for various accessories. With payment integration. Dont worry its in test mode",
+    stacks: ["React", "Redux", "Firebase", "HTML5", "CSS3", "JavaScript"],
+    link: "https://haxxstore.netlify.com/",
+    github: "https://github.com/Makwe-O/HaxxStore",
   },
   {
-    id: 4,
+    id: uuidv4(),
     name: `Store Manager`,
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1573508648/IMG_4466_buzkhn.jpg',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1573508648/IMG_4466_buzkhn.jpg",
     description:
-      'Take control of your online store. Create products and categories. Sell your products and create sales records',
+      "Take control of your online store. Create products and categories. Sell your products and create sales records",
     stacks: [
-      'React',
-      'Redux',
-      'Nodejs',
-      'Express',
-      'HTML5',
-      'CSS3',
-      'JavaScript',
-      'Postgres',
+      "React",
+      "Redux",
+      "Nodejs",
+      "Express",
+      "HTML5",
+      "CSS3",
+      "JavaScript",
+      "Postgres",
     ],
-    link: 'https://storemangerreact.herokuapp.com/',
-    github: 'https://github.com/Makwe-O/store-manager-react',
+    link: "https://storemangerreact.herokuapp.com/",
+    github: "https://github.com/Makwe-O/store-manager-react",
   },
   {
-    id: 5,
+    id: uuidv4(),
     name: `Weather Man`,
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1573508648/Untitled-1_n5vsye.jpg',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1573508648/Untitled-1_n5vsye.jpg",
     description:
-      'Never be caught off guard when it comes to weather. Weather Man has your back. Get the weather from a reliable source. ',
-    stacks: ['React', 'Axios', 'HTML5', 'CSS3', 'JavaScript'],
-    link: 'https://makwe-o.github.io/WeatherMan/',
-    github: 'https://github.com/Makwe-O/WeatherMan',
+      "Never be caught off guard when it comes to weather. Weather Man has your back. Get the weather from a reliable source. ",
+    stacks: ["React", "Axios", "HTML5", "CSS3", "JavaScript"],
+    link: "https://makwe-o.github.io/WeatherMan/",
+    github: "https://github.com/Makwe-O/WeatherMan",
   },
   {
-    id: 6,
+    id: uuidv4(),
     name: `Cadawada`,
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1587552457/CadaWada_-_Rhics_Technology_pzvulg.png',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1587552457/CadaWada_-_Rhics_Technology_pzvulg.png",
     description:
-      'CadaWada aims to seamlessly connect emerging markets to the global financial ecosystem through an API driven platform. It is a one-stop money shop for financial services',
-    stacks: ['TypeScript, NextJS, Bootstrap 4'],
-    link: 'https://rhicstech.com/cadawada/?currency=NGN',
+      "CadaWada aims to seamlessly connect emerging markets to the global financial ecosystem through an API driven platform. It is a one-stop money shop for financial services",
+    stacks: ["TypeScript, NextJS, Bootstrap 4"],
+    link: "https://rhicstech.com/cadawada/?currency=NGN",
     github: null,
   },
   {
-    id: 7,
-    name: 'Team Timer',
+    id: uuidv4(),
+    name: "Team Timer",
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1587552450/team-viewer-v1_uminej.png',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1587552450/team-viewer-v1_uminej.png",
     description:
       "Avoid Team Members wasting time on stand-ups when y'all can be doing productive work",
-    stacks: ['HTML5, CSS3, Tailwind CSS, Vuejs', 'Firebase'],
-    link: 'https://team-timerv1.netlify.app/',
+    stacks: ["HTML5, CSS3, Tailwind CSS, Vuejs", "Firebase"],
+    link: "https://team-timerv1.netlify.app/",
 
-    github: 'https://github.com/Makwe-O/Team-Viewer',
+    github: "https://github.com/Makwe-O/Team-Viewer",
   },
   {
-    id: 8,
-    name: 'View(Vue) Notes',
+    id: uuidv4(),
+    name: "View(Vue) Notes",
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1587552450/vue-notes-app_zgzvk9.png',
-    description: 'Keep track of notes for different subjects',
-    stacks: ['HTML5, CSS3, Vuejs'],
-    link: 'https://vuenotes.netlify.com/',
-    github: 'https://github.com/Makwe-O/Vue-Notes',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1587552450/vue-notes-app_zgzvk9.png",
+    description: "Keep track of notes for different subjects",
+    stacks: ["HTML5, CSS3, Vuejs"],
+    link: "https://vuenotes.netlify.com/",
+    github: "https://github.com/Makwe-O/Vue-Notes",
   },
   {
-    id: 9,
-    name: 'Converve',
+    id: uuidv4(),
+    name: "Converve",
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1588368554/Dark_Tablet_wjpdic.png',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1588368554/Dark_Tablet_wjpdic.png",
     description:
-      'Converge is a room management platform that enables users book and manage meeting rooms. Users of this platform can decide who attends a meeting, schedule meetings, manage resources in a meeting room such as white boards and marksers, speakers, monitors etc. ',
-    stacks: ['Python, Flask, GraphQL'],
+      "Converge is a room management platform that enables users book and manage meeting rooms. Users of this platform can decide who attends a meeting, schedule meetings, manage resources in a meeting room such as white boards and marksers, speakers, monitors etc. ",
+    stacks: ["Python, Flask, GraphQL"],
     link: null,
     github: null,
   },
   {
-    id: 10,
-    name: 'WeGlobalNow',
+    id: uuidv4(),
+    name: "WeGlobalNow",
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1589035171/restcountry_skuc75.png',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1589035171/restcountry_skuc75.png",
     description:
-      'Web Application that shows the details of countries around the world',
-    stacks: ['React, SASS, Styled Components'],
-    link: 'https://weglobalnow.netlify.app/',
-    github: 'https://github.com/Makwe-O/react-storybook',
+      "Web Application that shows the details of countries around the world",
+    stacks: ["React, SASS, Styled Components"],
+    link: "https://weglobalnow.netlify.app/",
+    github: "https://github.com/Makwe-O/react-storybook",
   },
   {
-    id: 11,
-    name: 'PWA BitCoin Converter',
+    id: uuidv4(),
+    name: "PWA BitCoin Converter",
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1589658446/PWA_v5gqud.png',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1589658446/PWA_v5gqud.png",
     description:
-      'Web Application shows the equivalent bitcoin convertion of various currencies',
-    stacks: ['React, ChakraUI'],
-    link: 'https://pwa-converter.notmaks.now.sh',
-    github: 'https://github.com/Makwe-O/PWA---Converter',
+      "Web Application shows the equivalent bitcoin convertion of various currencies",
+    stacks: ["React, ChakraUI"],
+    link: "https://pwa-converter.notmaks.now.sh",
+    github: "https://github.com/Makwe-O/PWA---Converter",
   },
   {
-    id: 12,
-    name: 'Eden Dog Ceo',
+    id: uuidv4(),
+    name: "Eden Dog Ceo",
     img:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1594499459/dog_mtxzke.png',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/e_grayscale/v1594499459/dog_mtxzke.png",
     description:
-      'A web app for all dog lovers. It leverages the dog ceo api to fetch cute pictures of dogs to warm your heart',
-    stacks: ['React, HTML, CSS, SASS'],
-    link: 'https://eden-dog-ceo.netlify.app/',
-    github: 'https://github.com/Makwe-O/eden-dog-ceo',
+      "A web app for all dog lovers. It leverages the dog ceo api to fetch cute pictures of dogs to warm your heart",
+    stacks: ["React, HTML, CSS, SASS"],
+    link: "https://eden-dog-ceo.netlify.app/",
+    github: "https://github.com/Makwe-O/eden-dog-ceo",
   },
 ];
 
 export const icons = [
   {
-    id: 101,
-    name: 'github',
+    id: uuidv4(),
+    name: "github",
     url:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/github-6003_26523480-1fbc-45e2-a938-b6d6533ec56e.svg',
-    link: 'https://github.com/Makwe-O',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/github-6003_26523480-1fbc-45e2-a938-b6d6533ec56e.svg",
+    link: "https://github.com/Makwe-O",
   },
   {
-    id: 102,
-    name: 'linkedin',
+    id: uuidv4(),
+    name: "linkedin",
     url:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/linkedin-6017_dbabf976-ca81-4e11-9a41-863fc65fc849.svg',
-    link: 'https://www.linkedin.com/in/mmakwe-onyeka-02507189/',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/linkedin-6017_dbabf976-ca81-4e11-9a41-863fc65fc849.svg",
+    link: "https://www.linkedin.com/in/mmakwe-onyeka-02507189/",
   },
   {
-    id: 103,
-    name: 'codepen',
+    id: uuidv4(),
+    name: "codepen",
     url:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/codepen-5982_73693040-bed4-4d57-acde-ad1b133617ce.svg',
-    link: 'https://codepen.io/notmaks',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/codepen-5982_73693040-bed4-4d57-acde-ad1b133617ce.svg",
+    link: "https://codepen.io/notmaks",
   },
   {
-    id: 104,
-    name: 'instagram',
+    id: uuidv4(),
+    name: "instagram",
     url:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/instagram-6054_f3ee122c-103f-441b-80fa-66a175c9ab9c.svg',
-    link: 'https://www.instagram.com/notmaks_/',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/instagram-6054_f3ee122c-103f-441b-80fa-66a175c9ab9c.svg",
+    link: "https://www.instagram.com/notmaks_/",
   },
   {
-    id: 105,
-    name: 'twitter',
+    id: uuidv4(),
+    name: "twitter",
     url:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/twitter-6033_d8128eb9-fd73-451d-8169-a8c7999ee355.svg',
-    link: 'https://twitter.com/Maks_onyeka',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/twitter-6033_d8128eb9-fd73-451d-8169-a8c7999ee355.svg",
+    link: "https://twitter.com/Maks_onyeka",
   },
   {
     id: 106,
-    name: 'mail',
+    name: "mail",
     url:
-      'https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/envelope-4708_172cdce8-74aa-4f01-9e82-dc40fdf12021.svg',
-    link: 'mmakweonyeka@gmail.com',
-    mail: 'mmakweonyeka@gmail.com',
+      "https://res.cloudinary.com/dnavbc7ny/image/upload/v1573566530/porfolio/envelope-4708_172cdce8-74aa-4f01-9e82-dc40fdf12021.svg",
+    link: "mmakweonyeka@gmail.com",
+    mail: "mmakweonyeka@gmail.com",
   },
 ];
 
 export const skills = [
   {
-    id: 1001,
-    name: 'React',
+    id: uuidv4(),
+    name: "JavaScript",
   },
   {
-    id: 1000,
-    name: 'React Native',
+    id: uuidv4(),
+    name: "TypeScript",
+  },
+  {
+    id: uuidv4(),
+    name: "React",
   },
 
   {
-    id: 1002,
-    name: 'Vue.js',
+    id: uuidv4(),
+    name: "Vue.js",
+  },
+
+  {
+    id: uuidv4(),
+    name: "Express.js",
   },
   {
-    id: 1003,
-    name: 'Redux',
+    id: uuidv4(),
+    name: "HTML 5/CSS3",
+  },
+
+  {
+    id: uuidv4(),
+    name: "Nodejs",
   },
   {
-    id: 1004,
-    name: 'Express.js',
-  },
-  {
-    id: 1005,
-    name: 'HTML 5/CSS3',
-  },
-  // {
-  //   id: 1006,
-  //   name: 'CSS3',
-  // },
-  {
-    id: 1007,
-    name: 'JavaScript',
-  },
-  {
-    id: 1008,
-    name: 'Nodejs',
-  },
-  {
-    id: 1009,
-    name: 'Python',
+    id: uuidv4(),
+    name: "Python",
   },
 ];
